278dbe5a3b731e477f7cf0d2fd981221bafbf7e2
jordan-bird <birdj1@aston.ac.uk>
2019-11-14 19:04:43 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,36 @@ import os
 import re
 from py3votecore.schulze_method import SchulzeMethod
 from py3votecore.condorcet import CondorcetHelper
+from py3votecore.ranked_pairs import RankedPairs
 from time import sleep
 
+choice = 0
+
+def intro():
+	global choice
+	print("Schulze [1] or Ranked Pairs [2]?")
+	selection = input()
+	print(selection)
+	if (selection!="1" and selection!="2"):
+		print("Choose [1] or [2]")
+		intro()
+	else:
+		if(selection=="1"):
+			print("Using Schultze Method")
+			choice = 1
+		if(selection=="2"):
+			print("Using Ranked Pairs Method")
+			choice = 2
+
+intro()
+
 def vote(ballots):
-	output = SchulzeMethod(ballots, ballot_notation = CondorcetHelper.BALLOT_NOTATION_GROUPING).as_dict()
+	output=""
+	if(choice==1):
+		output = SchulzeMethod(ballots, ballot_notation = CondorcetHelper.BALLOT_NOTATION_GROUPING).as_dict()
+	if(choice==2):
+		output = RankedPairs(ballots, ballot_notation=RankedPairs.BALLOT_NOTATION_GROUPING).as_dict()
+	
 	if 'tied_winners' in output.keys():
 		#print(output['tied_winners'])
 		return output['tied_winners']
@@ -41,26 +67,25 @@ dir_path += '\lists\\'
 print("Getting lists from: " + dir_path + "\n")
 num_files = len([f for f in os.listdir(dir_path)if os.path.isfile(os.path.join(dir_path, f))])
 print("There are " + str(num_files) + " lists!")
-
-
 print("\nBegin voting...\n")
-
 winners=[]
-for x in range(0, 25):
+for x in range(0, 50):
 	if(x==0):
 		ballots = getBallots()
 	else:
 		ballots = getBallots(winners)
-	winner = vote(ballots)
-	print(str(x+1) + ": " + str(winner))
-	
-	#print(type(winner))
-	
-	if type(winner) is set:
-		for val in winner:
-			winners.append(val)
-	else:
-		winners.append(winner)
-		
+		try:
+			winner = vote(ballots)
+			print(str(x+1) + ": " + str(winner))
+			
+			#print(type(winner))
+			
+			if type(winner) is set:
+				for val in winner:
+					winners.append(val)
+			else:
+				winners.append(winner)
+		except:
+			sleep(0) #lazy way of preventing EOF
 	
 	
